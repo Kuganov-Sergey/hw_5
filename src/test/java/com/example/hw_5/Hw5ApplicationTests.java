208105beef3daeb49ff65f3ba9923fb3d862e6ba
Kuganov-Sergey <bfgooodrich@mail.ru>
2022-07-20 19:42:14 +0300

    Добавил H2 test

--- src/test/java/com/example/hw_5/Hw5ApplicationTests.java
+++ src/test/java/com/example/hw_5/Hw5ApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.hw_5;
 
+import com.example.hw_5.service.RestaurantService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class Hw5ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private RestaurantService restaurantService;
 
 }
--- src/test/java/com/example/hw_5/config/AppContextTest.java
+++ src/test/java/com/example/hw_5/config/AppContextTest.java
@@ -1,7 +1,10 @@
 package com.example.hw_5.config;
 
 import com.example.hw_5.entity.Restaurant;
+import com.example.hw_5.service.RestaurantServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -11,7 +14,21 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 public class AppContextTest {
 
+    @Autowired
+    private RestaurantServiceImpl restaurantService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void getRestaurantById() {
+        String name = "lolo";
+        Restaurant restaurant = new Restaurant();
+        restaurant.setName(name);
+        restaurant.setDescription("apapapa");
+        restaurantService.addRestaurant(restaurant);
+        String newName = restaurantService.getDescriptionByName("lolo");
+        Assertions.assertEquals("apapapa", newName);
     }
 }
